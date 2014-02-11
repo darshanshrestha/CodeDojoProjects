968c22b384a9253a6a4802231142309f1b9d7275
PlanoCodeDojo <planocodedojo@gmail.com>
2014-02-11 15:43:49 -0600

    Added the tennis kata

---
+++ PracticeKata/given_a_tennis_game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PracticeKata
+{
+    [TestFixture]
+	public class given_a_tennis_game
+	{
+        [Test]
+        public void when_condition_then_expectation()
+        {
+            //Arrange
+             
+            //Act
+
+            //Assert
+            Assert.That(true, Is.EqualTo(true));
+        }
+	}
+}
